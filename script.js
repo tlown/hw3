f1270dc6954af3133aea3ae0015a11d07b7d11fc
Tyler Lown <tylrlown@yahoo.com>
2020-06-28 00:55:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function generate(){
+
+
+    let complexity = document.getElementById("slider").value;
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+
+    let password = "";
+
+    for (var i = 0; i <= complexity; i ++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+
+    document.getElementById("display").value = password;
+}
+
+document.getElementById("length").innerHTML = "length = 68";
+
+document.getElementById("slider").oninput = function(){
+    
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length" + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+}
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to the clipboard");
+}
